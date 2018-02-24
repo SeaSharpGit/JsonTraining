826820b384e1f9df21fbb0d51d275e0d18af84b6
dahai <1138538963@qq.com>
2018-02-25 00:17:17 +0800

    ReadMe图片

--- JsonTraining/Models/TestModel.cs
+++ JsonTraining/Models/TestModel.cs
@@ -25,160 +25,159 @@ namespace JsonTraining.Models
         private void ResetProperties()
         {
             #region 整形
-            //SByte = 123;
-            //SByteNullable = 123;
-            //Byte = 123;
-            //ByteNullable = 123;
-            //Int16 = 123;
-            //Int16Nullable = 123;
-            //UInt16 = 123;
-            //UInt16Nullable = 123;
-            //Int32 = 123;
-            //Int32Nullable = 123;
-            //UInt32 = 123U;
-            //UInt32Nullable = 123U;
-            //Int64 = 123L;
-            //Int64Nullable = 123L;
-            //UInt64 = 123UL;
-            //UInt64Nullable = 123UL;
+            SByte = 123;
+            SByteNullable = 123;
+            Byte = 123;
+            ByteNullable = 123;
+            Int16 = 123;
+            Int16Nullable = 123;
+            UInt16 = 123;
+            UInt16Nullable = 123;
+            Int32 = 123;
+            Int32Nullable = 123;
+            UInt32 = 123U;
+            UInt32Nullable = 123U;
+            Int64 = 123L;
+            Int64Nullable = 123L;
+            UInt64 = 123UL;
+            UInt64Nullable = 123UL;
             #endregion
 
             #region 浮点型
-            //Double = 1.23;
-            //DoubleNullable = 1.23;
-            //Float = 1.23F;
-            //FloatNullable = 1.23F;
-            //Decimal = 1.23M;
-            //DecimalNullable = 1.23M;
+            Double = 1.23;
+            DoubleNullable = 1.23;
+            Float = 1.23F;
+            FloatNullable = 1.23F;
+            Decimal = 1.23M;
+            DecimalNullable = 1.23M;
             #endregion
 
             #region 时间类型
-            //DateTime = new DateTime(2000, 1, 1);
-            //DateTimeNullable = new DateTime(2000, 1, 1);
-            //DateTimeOffset = new DateTimeOffset(new DateTime(2000, 1, 1));
-            //DateTimeOffset = new DateTimeOffset(new DateTime(2000, 1, 1));
-            //DateTimeOffsetNullable = new DateTimeOffset(new DateTime(2000, 1, 1));
-            //TimeSpan = new TimeSpan(1, 1, 1, 1);
-            //TimeSpanNullable = new TimeSpan(1, 1, 1, 1);
+            DateTime = new DateTime(2000, 1, 1);
+            DateTimeNullable = new DateTime(2000, 1, 1);
+            DateTimeOffset = new DateTimeOffset(new DateTime(2000, 1, 1));
+            DateTimeOffset = new DateTimeOffset(new DateTime(2000, 1, 1));
+            DateTimeOffsetNullable = new DateTimeOffset(new DateTime(2000, 1, 1));
+            TimeSpan = new TimeSpan(1, 1, 1, 1);
+            TimeSpanNullable = new TimeSpan(1, 1, 1, 1);
             #endregion
 
             #region 变量
-            //Filed = 1;
+            Filed = 1;
             #endregion
 
             #region 集合类型
-            //List = new List<int>() { 1, 2, 3 };
-            //List2 = new List<string>() { "1", "2", "3" };
-            //Array = new int[] { 1, 2, 3 };
-            //Array2 = new string[] { "1", "2", "3" };
-            //ArrayList = new ArrayList() { 1, "2", true };
+            List = new List<int>() { 1, 2, 3 };
+            List2 = new List<string>() { "1", "2", "3" };
+            Array = new int[] { 1, 2, 3 };
+            Array2 = new string[] { "1", "2", "3" };
+            ArrayList = new ArrayList() { 1, "2", true };
             Dictionary = new Dictionary<int, string>() { { 1, "2" }, { 3, "4" }, { 5, "6" } };
 
-            //var dt = new DataTable();
-            //dt.Columns.Add("Column0", typeof(bool));
-            //dt.Columns.Add("Column1");
-            //dt.Rows.Add(true, 2);
-            //dt.Rows.Add(false, 4);
-            //DataTable = dt;
+            var dt = new DataTable();
+            dt.Columns.Add("Column0", typeof(bool));
+            dt.Columns.Add("Column1");
+            dt.Rows.Add(true, 2);
+            dt.Rows.Add(false, 4);
+            DataTable = dt;
 
-            //DataSet = new DataSet();
-            //var dt2 = new DataTable("MyDataTable");
-            //dt2.Columns.Add("Column0");
-            //dt2.Columns.Add("Column1");
-            //dt2.Rows.Add(1, 2);
-            //dt2.Rows.Add(3, 4);
-            //DataSet.Tables.Add(dt2);
-            //var dt3 = new DataTable("MyDataTable2");
-            //dt3.Columns.Add("Column0");
-            //dt3.Columns.Add("Column1");
-            //dt3.Rows.Add(5, 6);
-            //dt3.Rows.Add(7, 8);
-            //DataSet.Tables.Add(dt3);
+            DataSet = new DataSet();
+            var dt2 = new DataTable("MyDataTable");
+            dt2.Columns.Add("Column0");
+            dt2.Columns.Add("Column1");
+            dt2.Rows.Add(1, 2);
+            dt2.Rows.Add(3, 4);
+            DataSet.Tables.Add(dt2);
+            var dt3 = new DataTable("MyDataTable2");
+            dt3.Columns.Add("Column0");
+            dt3.Columns.Add("Column1");
+            dt3.Rows.Add(5, 6);
+            dt3.Rows.Add(7, 8);
+            DataSet.Tables.Add(dt3);
             #endregion
 
             #region 其他类型
-            //String = "字符串{[\"";
+            String = "字符串{[\"";
             //Object = new { Name = "对象", Age = 20 };
-            //Bool = true;
-            //BoolNullable = true;
-            //Char = '1';
-            //CharNullable = '1';
-            //Guid = new Guid();
-            //GuidNullable = new Guid();
-            //BigInteger = new BigInteger(1);
-            //BigIntegerNullable = new BigInteger(1);
-            //Uri = new Uri("https://www.baidu.com");
-            //Enum = EnumModel.Second;
+            Bool = true;
+            BoolNullable = true;
+            Char = '1';
+            CharNullable = '1';
+            Guid = new Guid();
+            GuidNullable = new Guid();
+            BigInteger = new BigInteger(1);
+            BigIntegerNullable = new BigInteger(1);
+            Uri = new Uri("https://www.baidu.com");
+            Enum = EnumModel.Second;
             #endregion
 
         }
 
         #region 整形
-        //public sbyte SByte { get; set; }
-        //public sbyte SByteNullable { get; set; }
-        //public byte Byte { get; set; }
-        //public byte ByteNullable { get; set; }
-        //public short Int16 { get; set; }
-        //public short? Int16Nullable { get; set; }
-        //public ushort UInt16 { get; set; }
-        //public ushort? UInt16Nullable { get; set; }
-        //public int Int32 { get; set; }
-        //public int? Int32Nullable { get; set; }
-        //public uint UInt32 { get; set; }
-        //public uint? UInt32Nullable { get; set; }
-        //public long Int64 { get; set; }
-        //public long? Int64Nullable { get; set; }
-        //public ulong UInt64 { get; set; }
-        //public ulong? UInt64Nullable { get; set; }
+        public sbyte SByte { get; set; }
+        public sbyte SByteNullable { get; set; }
+        public byte Byte { get; set; }
+        public byte ByteNullable { get; set; }
+        public short Int16 { get; set; }
+        public short? Int16Nullable { get; set; }
+        public ushort UInt16 { get; set; }
+        public ushort? UInt16Nullable { get; set; }
+        public int Int32 { get; set; }
+        public int? Int32Nullable { get; set; }
+        public uint UInt32 { get; set; }
+        public uint? UInt32Nullable { get; set; }
+        public long Int64 { get; set; }
+        public long? Int64Nullable { get; set; }
+        public ulong UInt64 { get; set; }
+        public ulong? UInt64Nullable { get; set; }
         #endregion
 
         #region 浮点型
-        //public double Double { get; set; }
-        //public double? DoubleNullable { get; set; }
-        //public float Float { get; set; }
-        //public float? FloatNullable { get; set; }
-        //public decimal Decimal { get; set; }
-        //public decimal? DecimalNullable { get; set; }
+        public double Double { get; set; }
+        public double? DoubleNullable { get; set; }
+        public float Float { get; set; }
+        public float? FloatNullable { get; set; }
+        public decimal Decimal { get; set; }
+        public decimal? DecimalNullable { get; set; }
         #endregion
 
         #region 时间类型
-        //public DateTime DateTime { get; set; }
-        //public DateTime? DateTimeNullable { get; set; }
-        //public DateTimeOffset DateTimeOffset { get; set; }
-        //public DateTimeOffset? DateTimeOffsetNullable { get; set; }
-        //public TimeSpan TimeSpan { get; set; }
-        //public TimeSpan? TimeSpanNullable { get; set; }
+        public DateTime DateTime { get; set; }
+        public DateTime? DateTimeNullable { get; set; }
+        public DateTimeOffset DateTimeOffset { get; set; }
+        public DateTimeOffset? DateTimeOffsetNullable { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public TimeSpan? TimeSpanNullable { get; set; }
         #endregion
 
         #region 变量
-        //public int Filed;
+        public int Filed;
         #endregion
 
         #region 集合类型类型
-        //public List<int> List { get; set; }
-        //public List<string> List2 { get; set; }
-        //public int[] Array { get; set; }
-        //public string[] Array2 { get; set; }
-        //public ArrayList ArrayList { get; set; } = new ArrayList();
+        public List<int> List { get; set; }
+        public List<string> List2 { get; set; }
+        public int[] Array { get; set; }
+        public string[] Array2 { get; set; }
+        public ArrayList ArrayList { get; set; } = new ArrayList();
         public Dictionary<int, string> Dictionary { get; set; }
-
-        //public DataTable DataTable { get; set; }
-        //public DataSet DataSet { get; set; }
+        public DataTable DataTable { get; set; }
+        public DataSet DataSet { get; set; }
         #endregion
 
         #region 其他类型
-        //public string String { get; set; }
-        //public object Object { get; set; }
-        //public bool Bool { get; set; }
-        //public bool? BoolNullable { get; set; }
-        //public char Char { get; set; }
-        //public char? CharNullable { get; set; }
-        //public Guid Guid { get; set; }
-        //public Guid? GuidNullable { get; set; }
-        //public BigInteger BigInteger { get; set; }
-        //public BigInteger? BigIntegerNullable { get; set; }
-        //public Uri Uri { get; set; }
-        //public EnumModel Enum { get; set; } = EnumModel.Second;
+        public string String { get; set; }
+        public object Object { get; set; }
+        public bool Bool { get; set; }
+        public bool? BoolNullable { get; set; }
+        public char Char { get; set; }
+        public char? CharNullable { get; set; }
+        public Guid Guid { get; set; }
+        public Guid? GuidNullable { get; set; }
+        public BigInteger BigInteger { get; set; }
+        public BigInteger? BigIntegerNullable { get; set; }
+        public Uri Uri { get; set; }
+        public EnumModel Enum { get; set; } = EnumModel.Second;
         #endregion
 
     }
--- JsonTraining/Program.cs
+++ JsonTraining/Program.cs
@@ -2,7 +2,9 @@
 using JsonTraining.Models;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 
 namespace JsonTraining
 {
@@ -10,29 +12,70 @@ namespace JsonTraining
     {
         static void Main(string[] args)
         {
+            //var obj5 = new { Name = "123", Age = 456 };
+            //var strrr= JsonHelper.ConvertToJson(obj5);
+            //var objjj= JsonConvert.DeserializeObject<object>(strrr);
+
             #region 序列化
             var model = new TestModel(1);
-            Console.WriteLine("*********************************");
-            Console.WriteLine("JsonTraining测试结果");
+            Console.WriteLine("*************JsonTraining测试结果**************");
+            Console.WriteLine("");
             var a = JsonHelper.ConvertToJson(model);
             Console.WriteLine(a);
-            Console.WriteLine("*********************************");
-            Console.WriteLine("Json.NET测试结果");
+            Console.WriteLine("*************Json.NET测试结果***********");
+            Console.WriteLine("");
             var b = JsonConvert.SerializeObject(model);
             Console.WriteLine(b);
             Console.WriteLine("*********************************");
             Console.WriteLine(a == b ? "相同" : "不相同");
-            Console.WriteLine("*********************************");
             #endregion
 
 
             #region 反序列化
-
-            var model2 = new TestModel(1);
-            var str = JsonHelper.ConvertToJson(model2);
-            Console.WriteLine("字符串是：" + str);
+            var str = JsonHelper.ConvertToJson(model);
             var c = JsonHelper.ConvertToJsonObject<TestModel>(str);
             var d = JsonConvert.DeserializeObject<TestModel>(str);
+            #endregion
+
+            #region 性能测试
+            Console.WriteLine("**************性能测试************");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var json = JsonHelper.ConvertToJson(model);
+            }
+            watch.Stop();
+            Console.WriteLine("JsonTraining序列化10000个对象耗时：" + watch.ElapsedMilliseconds + "毫秒");
+
+
+            watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var json = JsonConvert.SerializeObject(model);
+            }
+            watch.Stop();
+            Console.WriteLine("Json.NET序列化10000个对象耗时：" + watch.ElapsedMilliseconds + "毫秒");
+
+
+            watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var obj = JsonHelper.ConvertToJsonObject<TestModel>(str);
+            }
+            watch.Stop();
+            Console.WriteLine("JsonTraining反序列化10000个对象耗时：" + watch.ElapsedMilliseconds + "毫秒");
+
+            watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                var obj = JsonConvert.DeserializeObject<TestModel>(str);
+            }
+            watch.Stop();
+            Console.WriteLine("Json.NET反序列化10000个对象耗时：" + watch.ElapsedMilliseconds + "毫秒");
             #endregion
 
 
